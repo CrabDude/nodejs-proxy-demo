fc2f8357ffcd0cbfca606fe17e5d29d9663abe75
Adam Crabtree <dude@noderiety.com>
2015-04-24 09:45:09 -0700

    Fix arguments passing with -- wrt logfile

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
+let path = require('path')
 let http = require('http')
 let fs = require('fs')
 let request = require('request')
-let scheme = 'http://'
+let through = require('through')
 let argv = require('yargs')
   .default('host', '127.0.0.1')
   .argv
+let scheme = 'http://'
 let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
 let destinationUrl = argv.url || scheme + argv.host + ':' + port
-let logStream = argv.logfile ? fs.createWriteStream(argv.logfile) : process.stdout
+let logStream = argv.logfile ? fs.createWriteStream(path.join(__dirname, argv.logfile)) : process.stdout
 
 http.createServer((req, res) => {
-  logStream.write(`\nRequest received at: ${req.url}`)
+  logStream.write(`\nEcho request received at: ${req.url}:\n` + JSON.stringify(req.headers))
   for (let header in req.headers) {
     res.setHeader(header, req.headers[header])
   }
-  logStream.write('\n\n\nEcho Request:\n' + JSON.stringify(req.headers))
-  req.pipe(logStream)
+  through(req, logStream, {autoDestroy: false})
   req.pipe(res)
 }).listen(8000)
 
+logStream.write('Listening at http://127.0.0.1:8000')
+
 http.createServer((req, res) => {
-  logStream.write(`\nProxying request to: ${destinationUrl + req.url}`)
-  let url
+  let url = destinationUrl
   if (req.headers['x-destination-url']) {
     url = req.headers['x-destination-url']
     delete req.headers['x-destination-url']
-  } else {
-    url = destinationUrl + req.url
   }
-
   let options = {
-    method: req.method,
     headers: req.headers,
-    url: url
+    url: url + req.url
   }
-  logStream.write('\n\n\nProxy Request:\n' + JSON.stringify(req.headers))
-  req.pipe(logStream)
 
-  let downstreamResponse = req.pipe(request(options))
-  downstreamResponse.pipe(logStream)
-  downstreamResponse.pipe(res)
+  logStream.write(`\nRequest proxied to: ${url + req.url}: \n` + JSON.stringify(req.headers))
+  through(req, logStream, {autoDestroy: false})
+
+  let destinationResponse = req.pipe(request(options))
+
+  logStream.write(JSON.stringify(destinationResponse.headers))
+  destinationResponse.pipe(res)
+  through(destinationResponse, logStream, {autoDestroy: false})
 }).listen(8001)
