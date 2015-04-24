b477bae029e64d32f47d9d2f0370158de70ef144
Adam Crabtree <dude@noderiety.com>
2015-04-24 13:13:25 -0700

    Remove through and use pipeOptions

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ let path = require('path')
 let http = require('http')
 let fs = require('fs')
 let request = require('request')
-let through = require('through')
 let argv = require('yargs')
   .default('host', '127.0.0.1')
   .argv
 let scheme = 'http://'
 let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
 let destinationUrl = argv.url || scheme + argv.host + ':' + port
-let logStream = argv.logfile ? fs.createWriteStream(path.join(__dirname, argv.logfile)) : process.stdout
+let logStream = argv.log ? fs.createWriteStream(path.join(__dirname, argv.log)) : process.stdout
 
 http.createServer((req, res) => {
   logStream.write(`\nEcho request received at: ${req.url}:\n` + JSON.stringify(req.headers))
   for (let header in req.headers) {
     res.setHeader(header, req.headers[header])
   }
-  through(req, logStream, {autoDestroy: false})
-  req.pipe(res)
+  req.pipe(logStream, {end: false})
+  req.pipe(res, {end: false})
 }).listen(8000)
 
 logStream.write('Listening at http://127.0.0.1:8000')
@@ -34,11 +33,11 @@ http.createServer((req, res) => {
   }
 
   logStream.write(`\nRequest proxied to: ${url + req.url}: \n` + JSON.stringify(req.headers))
-  through(req, logStream, {autoDestroy: false})
+  req.pipe(logStream, {end: false})
 
-  let destinationResponse = req.pipe(request(options))
+  let destinationResponse = req.pipe(request(options), {end: false})
 
   logStream.write(JSON.stringify(destinationResponse.headers))
-  destinationResponse.pipe(res)
-  through(destinationResponse, logStream, {autoDestroy: false})
+  destinationResponse.pipe(res, {end: false})
+  destinationResponse.pipe(logStream, {end: false})
 }).listen(8001)
